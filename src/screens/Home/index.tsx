b8db64f2b326b72935e5ef5efeaf2774766d3153
Antonio Alves <antonio.jpsalves@gmail.com>
2023-12-09 11:18:01 -0300

    feat: utilizando css em componentes

--- src/screens/Home/index.tsx
+++ src/screens/Home/index.tsx
@@ -1,13 +1,30 @@
-import { Text, View } from 'react-native'
+import { Text, View, TextInput, TouchableOpacity } from 'react-native'
 import { StatusBar } from 'expo-status-bar';
 import { styles } from './styles'
 
 export function Home() {
+
+  function handleParticipantAdd() {
+    // return console.log('oi')
+  }
+
   return (
     <View style={styles.container}>
       <StatusBar style="light" />
       <Text style={styles.eventName}>Nome do evento</Text>
       <Text style={styles.eventDate}>sexta feira, 08 de novembro de 2023</Text>
+
+      <View style={styles.form}>
+        <TextInput
+          style={styles.input}
+          placeholder='Nome do participante'
+          placeholderTextColor="#6b6b6b"
+          keyboardType='default'
+        />
+        <TouchableOpacity style={styles.button} onPress={handleParticipantAdd}>
+          <Text style={styles.buttonText}> + </Text>
+        </TouchableOpacity>
+      </View>
     </View>
   );
 }
